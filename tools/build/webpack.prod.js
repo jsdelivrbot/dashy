73c57391d05354c00a3902f592ec9dcbfd22be35
Matthew McLeod <matt@sparkboom.com>
2018-10-23 22:30:49 +0100

    Fix deploy (#27)
    * Fix deploy
    * Add circle ci config

--- tools/build/webpack.prod.js
+++ tools/build/webpack.prod.js
@@ -1,51 +1,52 @@
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const zopfli = require('@gfx/zopfli');
 const paths = require('./paths');
 
 module.exports = {
-  module:{
-      rules: [
-          {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-          }
-      ]
-  },
-  plugins: [
-    new FaviconsWebpackPlugin({
-      logo: paths.entryFavicon,
-      prefix: 'icons-[hash]/',
-      persistentCache: true,
-      inject: true,
-      title: 'Dashy',
-      icons: {
-        android: true,
-        appleIcon: true,
-        appleStartup: true,
-        coast: false,
-        favicons: true,
-        firefox: true,
-        opengraph: false,
-        twitter: false,
-        yandex: false,
-        windows: false
-      }
-    }),
-    new UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new CompressionWebpackPlugin({
-      compressionOptions: {
-         numiterations: 15
-      },
-      algorithm(input, compressionOptions, callback) {
-        return zopfli.gzip(input, compressionOptions, callback);
-      },
-      filename: '[path].gz[query]',
-      test: /\.js$/,
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  ]
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new FaviconsWebpackPlugin({
+            logo: paths.entryFavicon,
+            prefix: 'icons-[hash]/',
+            persistentCache: true,
+            inject: true,
+            title: 'Dashy',
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false,
+            },
+        }),
+        new UglifyJsPlugin({
+            sourceMap: true,
+        }),
+        new CompressionWebpackPlugin({
+            compressionOptions: {
+                numiterations: 15,
+            },
+            algorithm(input, compressionOptions, callback) {
+                return zopfli.gzip(input, compressionOptions, callback);
+            },
+            filename: '[path].gz[query]',
+            test: /\.js$/,
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+    ],
 };
